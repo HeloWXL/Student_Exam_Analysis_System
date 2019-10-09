aac37eec3500e1d505bb6f21065815ee65db48d5
yechengchao <yechengchao9707@163.com>
2019-10-09 16:43:36 +0800

    成功解决填空题批量导入，快，叫我爸爸

--- src/main/java/com/mac/demo/controller/CompletionQuestionController.java
+++ src/main/java/com/mac/demo/controller/CompletionQuestionController.java
@@ -67,7 +67,6 @@ public class CompletionQuestionController {
     public  Map<String,Object>  importCompletionQuestion(@RequestParam("file") MultipartFile file) {
         String fileName = file.getOriginalFilename();
         String pattern = fileName.substring(fileName.lastIndexOf(".") + 1);
-        //System.out.println(fileName+"  "+pattern);
         List<List<String>> listContent = new ArrayList<>();
         String message = "导入成功";
 
